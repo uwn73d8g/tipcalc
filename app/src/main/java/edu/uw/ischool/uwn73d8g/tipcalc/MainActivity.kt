42b9a25d6112cd40ec389775e4cc3c9a507974bf
uwd73d8g <tina.zhuang.t.z@gmail.com>
2018-04-17 02:34:11 -0700

    added screenshot

--- app/src/main/java/edu/uw/ischool/uwn73d8g/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/uwn73d8g/tipcalc/MainActivity.kt
@@ -2,6 +2,8 @@ package edu.uw.ischool.uwn73d8g.tipcalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -18,17 +20,38 @@ class MainActivity : AppCompatActivity() {
         b1.isEnabled = false
 
         var money: EditText = findViewById(R.id.amount)
-        money.setOnClickListener {
-//            money.context("$" + money.text.toString())
-            b1.isEnabled = true
-        }
+        money.addTextChangedListener(object: TextWatcher {
+
+            override fun afterTextChanged(p0: Editable) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                var amount = s.toString()
+                if (!amount.startsWith("$")) {
+                    money.setText("$$s")
+                } else if (amount.isEmpty()) {
+                    money.setText("$0.00")
+                } else if (amount.contains(".")) {
+                    var num = amount.split(".")
+                    if (num[1].length > 2) {
+                        var splitNum= num[1].slice(0..1)
+                        money.setText(num[0] + "." + splitNum)
+                    }
+                }
+                b1.isEnabled = true
+                money.setSelection(money.text.length)
+            }
+        })
 
 
         b1.setOnClickListener {
-            val value = money.text.toString().toDouble() * 0.15
-            Toast.makeText(this@MainActivity, "$" + df2.format(value).toString(),
-                    Toast.LENGTH_LONG).show()
-        }
+                val value = money.text.toString().substring(1).toDouble() * 0.15
+                Toast.makeText(this@MainActivity, "$" + df2.format(value).toString(),
+                        Toast.LENGTH_LONG).show()
+            }
     }
 }
 
